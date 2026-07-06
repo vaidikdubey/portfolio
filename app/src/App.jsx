8259d85e3d53033a7b686688cb588ce9c659c4c2
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-06 16:11:05 +0530

    removed invalid downalods and added correct files

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 
 function App() {
     return (
         <>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </>
     );
 }
