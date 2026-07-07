92e1cb9670365c75836aa2b6c7cc722e74e6a191
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-07 20:37:09 +0530

    Added footer section

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { AboutSection } from "../components/AboutSection";
 import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
 import { ProjectsSection } from "../components/ProjectsSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/StarBackground";
 import { ThemeToggle } from "../components/ThemeToggle";
-
 
 export const Home = () => {
     return (
@@ -30,6 +30,7 @@ export const Home = () => {
             </main>
 
             {/* Footer */}
+            <Footer />
         </div>
     );
 };
