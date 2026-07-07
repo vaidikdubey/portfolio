a221e09a4616a4f48cadf46a92bd8937d536bd5a
Vaidik Dubey <vaidik26dubey@gmail.com>
2026-07-07 21:48:46 +0530

    Added success logs

--- app/src/components/ContactSection.jsx
+++ app/src/components/ContactSection.jsx
@@ -38,6 +38,8 @@ export const ContactSection = () => {
                 captchaToken,
             });
 
+            console.log(res);
+
             toast.success("Message sent successfully!");
 
             e.currentTarget.reset();
